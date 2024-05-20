1efa49314ba2d11da26162a2f49bfbe8fd65cd08
namprobs98 <namprobs98@gmail.com>
2024-05-20 11:37:23 +0700

    done

---
+++ src/javasubstring/JavaSubstring.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javasubstring;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Slime nightmare
+ */
+public class JavaSubstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+
+}
